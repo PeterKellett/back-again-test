1d7b806ede92d3a3d3d8ff82dd6e030fadfc5d1a
PeterKellett <peterwkellett@gmail.com>
2021-09-23 11:45:43 +0000

    Added flash messages and session cookie.

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, session
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET", "randomstring123")
 
 @app.route("/")
 def index():
+    flash("Flash message")
     return render_template("index.html")
 
 if __name__ == "__main__":
